d41ff62fa498f082b388ef98442264e8f1166053
tejus3131 <tejus3131@gmail.com>
2024-07-20 11:46:34 +0530

    Initial commit

---
+++ pm/__init__.py
@@ -0,0 +1,14 @@
+"""Password Manager package."""
+
+from pm.main import PasswordManager
+
+__all__ = ['PasswordManager']
+__version__ = '0.1.0'
+
+
+def main():
+    PasswordManager("PM")
+
+
+if __name__ == "__main__":
+    main()
